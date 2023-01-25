7c02121ad7cc4a5dc7ebe2a690c646fd2091b9a8
ISergienkoSF <sergienko.ivan2020@mail.ru>
2023-01-25 12:09:01 +0300

    group by date

--- app/src/main/java/com/viol4tsf/testfitnesskit/adapter/ScheduleAdapter.kt
+++ app/src/main/java/com/viol4tsf/testfitnesskit/adapter/ScheduleAdapter.kt
@@ -3,23 +3,23 @@ package com.viol4tsf.testfitnesskit.adapter
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.viol4tsf.testfitnesskit.databinding.LessonItemViewBinding
-import com.viol4tsf.testfitnesskit.model.ClassFitness
+import com.viol4tsf.testfitnesskit.model.DateLesson
 import com.viol4tsf.testfitnesskit.model.Lesson
 import com.viol4tsf.testfitnesskit.model.Trainer
-import retrofit2.Response
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.time.Duration
-import java.time.LocalTime
+import java.time.*
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class ScheduleAdapter: RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>() {
 
     var listTrainers = emptyList<Trainer>()
     var listLessons = emptyList<Lesson>()
+    private var dateNeeded = mutableListOf(true)
+    private var listDatesWithLesson = mutableListOf<DateLesson>()
 
     class ScheduleViewHolder(val itemBinding: LessonItemViewBinding): RecyclerView.ViewHolder(itemBinding.root)
 
@@ -33,11 +33,11 @@ class ScheduleAdapter: RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>(
 
     @SuppressLint("SimpleDateFormat")
     override fun onBindViewHolder(holder: ScheduleViewHolder, position: Int) {
-        val currentLesson = listLessons[position]
+        val currentLesson = listDatesWithLesson[position].lesson
         var coach = ""
         (1..listTrainers.size).forEach{
             if (currentLesson.coach_id == listTrainers[it-1].id){
-                coach = listTrainers[it].full_name
+                coach = listTrainers[it-1].full_name
             }
         }
 
@@ -46,7 +46,7 @@ class ScheduleAdapter: RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>(
         val duration = Duration.between(startTime, endTime)
         val asHours = duration.toHours()
         val asMinutes = duration.toMinutes() - asHours * 60
-        var finalDuration: String
+        val finalDuration: String
         if (asHours == 0L){
             finalDuration = "${asMinutes}мин."
         } else {
@@ -60,6 +60,12 @@ class ScheduleAdapter: RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>(
         holder.itemBinding.durationTextView.text = finalDuration
         holder.itemBinding.locationTextView.text = currentLesson.place
         holder.itemBinding.colorView.setBackgroundColor(Color.parseColor(currentLesson.color))
+        if (dateNeeded[position]){
+            holder.itemBinding.dateTextView.visibility = View.VISIBLE
+            holder.itemBinding.dateTextView.text = listDatesWithLesson[position].date
+        } else {
+            holder.itemBinding.dateTextView.visibility = View.GONE
+        }
     }
 
     override fun getItemCount(): Int {
@@ -68,8 +74,34 @@ class ScheduleAdapter: RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>(
 
     @SuppressLint("NotifyDataSetChanged")
     fun setList(listL: List<Lesson>, listC: List<Trainer>){
+        listL.sortedBy { it.date }
         listLessons = listL
         listTrainers = listC
+        createDateWithLessonList(listLessons)
+        createDateIfNeededList(listDatesWithLesson)
         notifyDataSetChanged()
+    }
+
+    private fun createDateIfNeededList(listD: List<DateLesson>){
+        (1 until listD.size).forEach{
+            if (listD[it].lesson.date != listD[it-1].lesson.date){
+                dateNeeded.add(true)
+            } else {
+                dateNeeded.add(false)
+            }
+        }
+    }
+
+    private fun createDateWithLessonList(listL: List<Lesson>){
+        (1 .. listL.size).forEach{
+            val str = "${listL[it-1].date} ${listL[it-1].startTime}"
+            val dateTime = LocalDateTime.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+            val date = LocalDate.parse(listL[it-1].date, DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            val dateTimeLong: Long =
+                dateTime.toEpochSecond(ZoneOffset.UTC)
+
+            listDatesWithLesson.add(DateLesson(dateTimeLong, date.toString(), listL[it-1]))
+        }
+        listDatesWithLesson.sortBy { it.dateTime }
     }
 }
---
+++ app/src/main/java/com/viol4tsf/testfitnesskit/model/DateLesson.kt
@@ -0,0 +1,9 @@
+package com.viol4tsf.testfitnesskit.model
+
+import java.time.LocalDateTime
+
+data class DateLesson(
+    val dateTime: Long,
+    val date: String,
+    val lesson: Lesson
+    )
